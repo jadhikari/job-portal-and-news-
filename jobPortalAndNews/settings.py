84adbbb23e38ae40bb3fb3bc3044441cbb6557c8
Jonam Adhikari <jadhikari131@gmail.com>
2025-02-24 23:11:02 +0900

    adding base url and core templates

--- jobPortalAndNews/settings.py
+++ jobPortalAndNews/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'core',
 ]
 
+
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
@@ -66,10 +68,11 @@ ROOT_URLCONF = 'jobPortalAndNews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add the global templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                # default context processors
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
